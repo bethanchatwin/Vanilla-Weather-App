644b71c14a663c96699224642c65176d0bec71b6
bethanchatwin <168184672+bethanchatwin@users.noreply.github.com>
2024-05-01 16:40:50 +0100

    created the temperature display function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ function handleSearchSubmit(event) {
   let str = searchInput.value;
   str = str.charAt(0).toUpperCase() + str.slice(1);
   cityElement.innerHTML = str;
+  handleSearch(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function displayTemperature(response) {
+  let temp = Math.round(response.data.temperature.current);
+  let tempElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = `${temp}`;
+}
+
+function handleSearch(city) {
+  let apiKey = "0083118ae860436btf146b9cbfb3oc7f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
